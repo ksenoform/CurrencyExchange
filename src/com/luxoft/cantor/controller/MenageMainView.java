28fb37e5c09308780c7b0203dc0349844a3ccacc
Ksenoform <ksenoform@gmail.com>
2015-11-23 21:53:03 +0100

    add page where user can delate one currency

--- src/com/luxoft/cantor/controller/MenageMainView.java
+++ src/com/luxoft/cantor/controller/MenageMainView.java
@@ -3,10 +3,12 @@ package com.luxoft.cantor.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.luxoft.cantor.repository.CurrencyRepository;
+import com.luxoft.cantor.support.GettingLonelyWalueFromPage;
 
 @Controller
 public class MenageMainView {
@@ -17,5 +19,28 @@ public class MenageMainView {
     public String showPageWitchRegistedCurrenciesAndActionButtons(Model model) {
         model.addAttribute("currencyRates", currencyRepository.getAllCurrency());
         return "forms/menageForm";
+    }
+    
+    @RequestMapping(value = "forms/deleteForm", method = RequestMethod.GET) 
+    public String showPageWitchFormWhoShowsDelatingCurrency(Model model) {
+        model.addAttribute("currenciesName", new GettingLonelyWalueFromPage());
+        return "forms/deleteForm";
+    }
+
+    @RequestMapping(value = "forms/AfterMenageForm", params = "Main Page", method = RequestMethod.POST) 
+    public String backToMainPage() {
+        return "redirect:/";
+    }
+    
+    @RequestMapping(value = "forms/AfterMenageForm", params = "OK", method = RequestMethod.POST) 
+    public String backToMainPageAfterDelating(@ModelAttribute ("hermit") String hermit) {
+        currencyRepository.deleteCurrencyByCode(hermit);
+        return "redirect:/";
+    }
+    
+    @RequestMapping(value = "forms/menageForm", params = "Delete currencies", method = RequestMethod.POST)
+    public String sendingToPageWitchFormWhoShowsDelatingCurrency(Model model) {
+        model.addAttribute("currenciesName", new GettingLonelyWalueFromPage());
+        return "forms/deleteForm";
     }
 }
--- src/com/luxoft/cantor/repository/CurrencyRepository.java
+++ src/com/luxoft/cantor/repository/CurrencyRepository.java
@@ -1,8 +1,10 @@
 package com.luxoft.cantor.repository;
 
 import java.util.List;
+import com.luxoft.cantor.repository.Dibs;
 
 public interface CurrencyRepository {
-    List<Dibs> getAllCurrency();
-    Dibs getCurrencyByCode(String code);
+    public List<Dibs> getAllCurrency();
+    public Dibs getCurrencyByCode(String code);
+    public void deleteCurrencyByCode(String code);
 }
