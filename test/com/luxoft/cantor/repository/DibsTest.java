165e59d65dc46b1c81d3911ce3c7576f051a377e
Robert <rszczygielski@luxoft.com>
2015-11-26 13:04:54 +0100

    Update Dibs calss by adding posibility to recive date about rates

---
+++ test/com/luxoft/cantor/repository/DibsTest.java
@@ -0,0 +1,43 @@
+package com.luxoft.cantor.repository;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DibsTest {
+	private Dibs dibs;
+	
+	@Before
+	public void setup() {
+		dibs = new Dibs.BuildDibs()
+					   .addCode("ABC")
+					   .addName("ABCDol")
+					   .addRate("3")
+					   .build();
+	}
+	
+	@Test
+	public void checkIfCodeIsABC() {
+		assertEquals("ABC", dibs.getDibsCode());
+	}
+	
+	@Test
+	public void checkIfNameIsABCDol() {
+		assertEquals("ABCDol", dibs.getDibsName());
+	}
+	
+	@Test
+	public void checkIfValueRateIsTheSameAsDefaultValueAndLastInDB() {
+		assertEquals(dibs.getDibsRate(), dibs.getRateAndDateOfLastDibs().getDibsRate());
+	}
+
+	@Test
+	public void checkIfRateVariableWillChangeAfterUpdateRate() {
+		dibs.setDibsRate(new BigDecimal(8));
+		assertEquals(dibs.getDibsRate(), dibs.getRateAndDateOfLastDibs().getDibsRate());
+	}
+	
+}
