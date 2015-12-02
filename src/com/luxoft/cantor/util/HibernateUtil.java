742654db606be56e135ad32123c1ebdf184f073a
Robert <rszczygielski@luxoft.com>
2015-12-02 15:47:31 +0100

    change the way to connection to  Database, replase database to MySQL, replase mapping files for hibernate to annotations

--- src/com/luxoft/cantor/util/HibernateUtil.java
+++ src/com/luxoft/cantor/util/HibernateUtil.java
@@ -1,28 +1,19 @@
 package com.luxoft.cantor.util;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory = buildSessionFactory();
-
-    private static SessionFactory buildSessionFactory() {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            Configuration configuration = new Configuration().configure();
-            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
-            applySettings(configuration.getProperties());
-            return configuration.buildSessionFactory(builder.build());
-//            return new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
+    public static void sendToDataBase(Object object) {
+    	EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myBase");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(object);
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
     }
 }
